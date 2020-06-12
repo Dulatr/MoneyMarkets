410716f5650a67eb302a6bd13c4b4a53271142a3
Dulatr <dulatr@appstate.edu>
2020-06-12 11:06:01 -0400

    Reformat argument descriptions

--- utils/Parser.py
+++ utils/Parser.py
@@ -4,14 +4,14 @@ def Parse(*args):
     parser = ap.ArgumentParser(prog="money",description="A toolkit for retrieving CNN Money Market webpage data.")
 
     # File arguments
-    suppress = parser.add_argument("--suppress","-s",action="store_true",help="Write data as file to /<program_directory>/data/*.json")
+    suppress = parser.add_argument("-s","--suppress",action="store_true",help="Suppress stdout. Write data as file to /<program_directory>/data/*.<format> (Default format is json).")
 
     # Data arguments
     app = parser.add_argument("app",default=["stock"],nargs='*',help="Return story/stock/currency data from webpages (Default is stock).")
-    img = parser.add_argument("--image","-i",dest='img',action="store_true",help="Return image data from speficied app.")
+    img = parser.add_argument("-i","--image",dest='img',action="store_true",help="Return image data from speficied app.")
 
     # Format arguments
-    _format = parser.add_argument("--format",'-f',nargs=1,default='json',help="Write data to stdout or a file in specified format (Default is json). See docs for valid types.")
+    _format = parser.add_argument('-f',"--format",nargs=1,default='json',help="Write data to stdout or a file in specified format (Default is json). See docs for valid types.")
 
     collection = ap.Namespace()  
     if not(args is None):
