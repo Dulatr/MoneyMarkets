7828578bb85c989a2ad0211ac42c9dc7f15d07c3
Dulatr <dulatr@appstate.edu>
2020-06-15 22:51:10 -0400

    Extend getTable to other indexes
    * Uses the new Tools module for common
      methods
    
    * Now includes type hints for future testing
    
    * markets keyword accepts a list of indexes
      to format and return
    
    * make keystats a flag for getTable() since
      it's so different in format.

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 from selenium import webdriver
+from Tools import (
+    isFailedResponse,
+    getTableElements,
+)
 
 class Stock:
 
@@ -24,19 +28,75 @@ class Stock:
         
         return overview_data
 
-    def getTable(self):
+    def getTable(self,keystats: bool = False,markets: list = None) -> dict:
+        """
+        Get a table specified from the markets endpoint and whether to include keystats table. 
+        """
+        url = "https://money.cnn.com/data"
+
+        response = {}
         DOW_composite_data ={}
-        self._client.get("https://money.cnn.com/data/dow30/")
-        dow30_table = self._client.find_element_by_css_selector('table.wsod_dataTableBig')
-        dow30_items = dow30_table.find_elements_by_tag_name('tr')
+        markets_data = {}
 
-        for item in dow30_items:
-            name = item.find_element_by_class_name('wsod_firstCol').get_attribute('innerText').split('\xa0')[0]
-            data = [txt.text for txt in item.find_elements_by_class_name("wsod_aRight")]
-            DOW_composite_data[name] = data
+        if not(markets is None):
+            try:
+                for item in markets:
+                    if not isinstance(item,str):
+                        raise TypeError(f"'{item}' in argument 'markets' expected '{type(str)}', received '{type(item)}'.")
+            except:
+                if not isinstance(markets,str):
+                    raise TypeError(f"'{markets}' in argument 'markets' expected '{type(str)}', received '{type(markets)}'.")
 
-        return DOW_composite_data
+        if isinstance(markets,list):
+            for item in markets:
+                if isFailedResponse(url + f"/markets/{item}/"):
+                    return {"Error":f"'{item}' in argument 'markets' not found."}
+                
+                # make a new dictionary
+                response[item] = {}
 
+                self._client.get(url + f"/markets/{item}/")
+
+                try:
+                    table_items = getTableElements(self._client,2)
+                    for table_item in table_items:
+                        row = table_item.get_attribute('innerText').split()
+                        response[item][row[0]] = row[1:]
+                except Exception as e:
+                    pass
+                    
+                try:
+                    self._client.find_element_by_id("fwd").click()
+                    table_items = getTableElements(self._client,2)
+                    for table_item in table_items:
+                        row = thing.get_attribute('innerText').split()
+                        response[item][row[0]] = row[1:]
+                except Exception as e:
+                    pass
+        else:
+            self._client.get(url + f"/markets/{markets}/")
+            
+            try:
+                table_items = getTableElements(self._client,2)
+                for table_item in table_items:
+                    row = table_item.get_attribute('innerText').split()
+                    response[item][row[0]] = row[1:]
+            except Exception as e:
+                print(e)
+
+        if keystats:
+            self._client.get(url + "/dow30/")
+            dow30_items = getTableElements(self._client)
+
+            for item in dow30_items:
+                name = item.find_element_by_class_name('wsod_firstCol').get_attribute('innerText').split('\xa0')[0]
+                data = [txt.text for txt in item.find_elements_by_class_name("wsod_aRight")]
+                DOW_composite_data[name] = data
+
+            response["keystats"] = DOW_composite_data
+        
+        return response
+    
     def getUpdated(self):
         self._client.get("https://money.cnn.com/data/markets/")
         response = self._client.find_element_by_css_selector("div.disclaimer").get_attribute('innerText')
