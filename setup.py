4f18370e9a8fb9013f5bfc79992330557a8f80c8
Dulatr <dulatr@appstate.edu>
2020-06-20 16:57:41 -0400

    Bring setup version up to date

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ options = {
 setup(
     name = "money",
     options = options,
-    version = "v0.2.2-a3",
+    version = "v0.2.3-a3",
     description = 'basic description',
     executables = executables
 )
